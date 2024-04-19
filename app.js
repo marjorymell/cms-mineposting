c439aa448b3089882ef259a3f2fc98e09f1d3411
Marjory Mel <marjorymel48@gmail.com>
2024-04-18 22:21:24 -0300

    booting the server

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
-//Requires 
-
+//Requires gerais
+const path = require("path")
+require("dotenv").config({ path: path.join(__dirname, "config", ".env") });
 
 //Express
-
+const express = require('express')
+const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')))
 
 //Template
-
+var mustacheExpress = require("mustache-express");
+var engine = mustacheExpress()
+app.engine("mustache", engine);
+app.set("views", path.join(__dirname, "views", "default"));
+app.set("view engine", "mustache");
 
 //Cookies
-
+const cookieParser = require("cookie-parser")
+app.use(cookieParser())
 
 //Sessão
+const session = require("express-session")
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
 
+app.use("/", require('./scr/controllers/authController'))
 
+app.listen(process.env.PORT, () => {
+    console.log("Running...")
+})
