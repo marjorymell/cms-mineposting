8ed2aeb78b4da88ef14471de43ebf9114be3f01c
Marjory Mel <marjorymel48@gmail.com>
2024-04-21 19:21:58 -0300

    edit validation

--- scr/routes/defaultRoutes.js
+++ scr/routes/defaultRoutes.js
@@ -1,22 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const { validateLogin, handleValidationErrors } = require('../../utils/validation')
 
 // Default
 router.get("/", (req, res) => {
-    console.log("Acessou a rota '/'");
+    console.log("Acessou a rota '/'")
     res.render("index");
 });
 
 //Login
 router.get("/login", (req, res) => {
-    console.log("Acessou a rota '/login'");
-    res.render("login");
+    console.log("Acessou a rota '/login'")
+    res.render("login")
 });
+
+router.post("/login", validateLogin, handleValidationErrors, (req, res) => {
+    const { email, password } = req.body
+    res.status(400).render('login', { errors: req.validationErrors() })
+  });
+  
 
 //Logout
 router.get("/logout", (req, res) => {
-    console.log("Acessou a rota '/logout");
-    res.render("index");
+    console.log("Acessou a rota '/logout")
+    res.render("index")
 });
 
 module.exports = router
--- utils/validation.js
+++ utils/validation.js
@@ -1,13 +1,22 @@
-const { body } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 
-exports.loginValidationRules = () => {
-  return [
-    body('email').isEmail().normalizeEmail(),
-    body('password').isLength({ min: 6 })
-  ];
-};
+// Conjunto de regras de validação para o formulário de login
+exports.validateLogin = [
+  body('email', 'O e-mail é obrigatório').trim().isEmail(),
+  body('password', 'A senha é obrigatória e deve ter no mínimo 6 caracteres').trim().isLength({ min: 6 }),
+];
 
+// Validção do admin
 exports.adminCredentials = {
   email: process.env.ADMIN,
   password: process.env.PASSWORD
+};
+
+// Middleware para processar os resultados da validação
+exports.handleValidationErrors = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
 };
