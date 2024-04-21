74bb52a3677e49d4eaa8edbb90d5658123a3d9a2
Marjory Mel <marjorymel48@gmail.com>
2024-04-21 20:13:35 -0300

    edit defaultRoutes

--- scr/routes/defaultRoutes.js
+++ scr/routes/defaultRoutes.js
@@ -2,19 +2,24 @@ const express = require('express');
 const router = express.Router();
 
 
-// Rota para renderizar o formulário de login
+router.get("/", (req, res) => {
+    console.log("Acessou a rota '/'");
+    res.render("login");
+});
+
+
 router.get("/login", (req, res) => {
     console.log("Acessou a rota '/login'");
     res.render("login");
 });
 
-// Rota para processar o formulário de login
+
 router.post("/login", (req, res) => {
     console.log("Acessou a rota '/login'");
     res.render("login");
 });
 
-// Rota para renderizar o formulário de login
+
 router.get("/logout", (req, res) => {
     console.log("Acessou a rota '/logout'");
     res.render("index");
