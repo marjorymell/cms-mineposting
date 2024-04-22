277403865469f600ff2ac7525af06619e394e3e3
Marjory Mel <marjorymel48@gmail.com>
2024-04-22 00:25:27 -0300

    installing express-session

--- scr/routes/defaultRoutes.js
+++ scr/routes/defaultRoutes.js
@@ -18,22 +18,21 @@ router.get("/login", (req, res) => {
 router.post("/login", (req, res) => {
     const { email, password } = req.body;
 
-    // Verificar se é um administrador
     if (checksAdmin(email, password)) {
-        // Se for um administrador, faça algo (por exemplo, redirecione para uma página de administração)
+        req.session.isAdmin = true;
         res.redirect("/");
     } else if (checksUser(email, password)) {
-        // Se for um usuário comum, faça algo (por exemplo, redirecione para a página de perfil do usuário)
+        req.session.isLoggedIn = true;
         res.redirect("/");
     } else {
         res.status(401).send("Credenciais inválidas");
     }
 });
 
-
 router.get("/logout", (req, res) => {
-    console.log("Acessou a rota '/logout'");
-    res.render("index");
+    // Destrua a sessão ao fazer logout
+    req.session.destroy();
+    res.redirect("/");
 });
 
 module.exports = router;
