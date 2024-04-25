979806e0fc8e88348494af68255c8b5e455f0655
Marjory Mel <marjorymel48@gmail.com>
2024-04-24 21:43:20 -0300

    middleware for checking admin routes added

--- scr/routes/adminRoutes.js
+++ scr/routes/adminRoutes.js
@@ -1,34 +1,38 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
+// Middleware for admin verification
+const isAdmin = (req, res, next) => {
+    if (req.session.isAdmin) {
+        // If the user is an admin, proceed to the next route
+        next();
+    } else {
+        // If not an admin, redirect to an error page or send an error message
+        res.status(403).send("Access denied. You are not an admin.");
+    }
+};
 
-// Route for accessing the admin dashboard
-router.get("/admin", (req, res) => {
-    const isLoggedIn = req.session.isLoggedIn || req.session.isAdmin;
-    const isAdmin = req.session.isAdmin || false; 
-    res.render("admin", { isLoggedIn, isAdmin })
+// Route to access the admin dashboard
+router.get("/admin", isAdmin, (req, res) => {
+    const isAdmin = req.session.isAdmin || false;
+    res.render("admin", { isAdmin });
 });
 
-// Route for accessing the posts page
-router.get("/posts", (req, res) => {
-    console.log("Acessou a rota '/posts'");
+// Routes related to posts
+router.get("/posts", isAdmin, (req, res) => {
+    console.log("Accessed the '/posts' route");
 });
 
-// Route for accessing the page to create a new post
-router.get("/posts/create", (req, res) => {
-    console.log("Acessou a rota '/posts/create'");
+router.get("/posts/create", isAdmin, (req, res) => {
+    console.log("Accessed the '/posts/create' route");
 });
 
-// Route for accessing the page to edit a post by ID
-router.get("/posts/edit/:id", (req, res) => {
-    console.log("Acessou a rota '/posts/create'");
+router.get("/posts/edit/:id", isAdmin, (req, res) => {
+    console.log("Accessed the '/posts/edit/:id' route");
 });
 
-// Route for accessing the page to delete a post by ID
-router.get("/posts/delete/:id", (req, res) => {
-    console.log("Acessou a rota '/posts/create'");
+router.get("/posts/delete/:id", isAdmin, (req, res) => {
+    console.log("Accessed the '/posts/delete/:id' route");
 });
 
-
-module.exports = router
-
+module.exports = router;
